ded4024830b1ddeb7466841c68b3217d91755379
Chris Francart <chris@visionpointmarketing.com>
2014-10-28 11:00:41 -0400

    - updated database  - styling for user login form

--- themes/starterkit/js/custom.js
+++ themes/starterkit/js/custom.js
@@ -2,4 +2,7 @@ jQuery( document ).ready(function () {
 	jQuery('#block-menu-block-2 .block__title').on('click',function(e){
 	    jQuery(this).siblings('.menu-block-wrapper').children('ul').toggleClass("open");
 	});
+	jQuery("#block-user-login .block__title").on('click',function(e){
+		jQuery(this).siblings('.block__content').toggleClass('open');
+	});
 });
