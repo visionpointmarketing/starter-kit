8eb26c5499556755b5edd94839afd82f40dcc7a0
Chris Francart <chris@visionpointmarketing.com>
2015-03-06 11:20:56 -0500

    removing debug info

--- themes/starterkit/templates/bean--text-and-links-promo.tpl.php
+++ themes/starterkit/templates/bean--text-and-links-promo.tpl.php
@@ -28,7 +28,6 @@
  */
 ?>
 
-<?php print_r($elements['field_text_links_image'][0]); ?>
 <div class="<?php print $classes; if(!isset($elements['field_text_links_image'][0])){echo ' noimg';} ?> a-<?php print($elements['field_alignment'][0]['#markup']); ?> clearfix"<?php print $attributes; ?>>
 	<div class="content <?php print $content_attributes ?>">
 		<?php if($title != ' '){ ?> <h2><?php print $title ?></h2> <?php } ?>
