5f2fbfa52b289af32b69eadf07a5a3d550400498
vagrant <vagrant@localhost>
2015-04-14 21:13:16 +0000

    update theme name from starterkit to unc_charlotte4

---
+++ themes/unc_charlotte4/templates/bean--grouped-cta.tpl.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * @file
+ * Default theme implementation for beans.
+ *
+ * Available variables:
+ * - $content: An array of comment items. Use render($content) to print them all, or
+ *   print a subset such as render($content['field_example']). Use
+ *   hide($content['field_example']) to temporarily suppress the printing of a
+ *   given element.
+ * - $title: The (sanitized) 'entity' label.
+ * - $url: Direct url of the current 'entity' if specified.
+ * - $page: Flag for the full page state.
+ * - $classes: String of classes that can be used to style contextually through
+ *   CSS. It can be manipulated through the variable $classes_array from
+ *   preprocess functions. By default the following classes are available, where
+ *   the parts enclosed by {} are replaced by the appropriate values:
+ *   - 'entity'-{'entity'_TYPE}
+ *   - {'entity'_TYPE}-{BUNDLE}
+ *
+ * Other variables:
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_'entity'()
+ * @see template_process()
+ */
+?>
+<div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+	<div class="content"<?php print $content_attributes; ?>>
+		<div class="header">
+			<h2><?php print $title; ?></h2>
+			<p><?php print_r($content['field_intro'][0]['#markup']); ?></p>
+		</div>
+		<?php
+		if(isset($content['field_cta'][0])){
+			if(isset($content['field_cta'][1])){
+				if(isset($content['field_cta'][2])){
+					$val = 3;
+				}else{
+					$val = 2;
+				}
+			}else{
+				$val = 1;
+			}
+		}else{
+			$val = 0;
+		}
+		//print_r($content['field_cta'][0]['entity']['field_collection_item'][4]['field_cta_intro']['#items']);
+		$string = "";
+		for($i = 0; $i <= $val - 1; $i++){
+			$fa_icon = $content['field_cta'][$i]['entity']['field_collection_item'][$i + $val + 1]['field_fontawesome_icon']['#items'][0]['value'];
+			$cta_title = $content['field_cta'][$i]['entity']['field_collection_item'][$i + $val + 1]['field_cta_title']['#items'][0]['title'];
+			$cta_link = $content['field_cta'][$i]['entity']['field_collection_item'][$i + $val + 1]['field_cta_title']['#items'][0]['url'];
+			$cta_intro = $content['field_cta'][$i]['entity']['field_collection_item'][$i + $val + 1]['field_cta_intro']['#items'][0]['value'];
+			$string .= "<div class='cta-content'><div class='icon'><i class='fa fa-".$fa_icon."''></i></div><a href='".$cta_link."'><div class='content'><h3>".$cta_title."</h3><p>".$cta_intro."</p></div></a></div>";
+		}
+			echo $string;
+		?>
+	</div>
+</div>
+
+
+
---
+++ themes/unc_charlotte4/theme-settings.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * @file
+ * Theme settings file for the unc_charlotte4 theme.
+ */
+
+require_once dirname(__FILE__) . '/template.php';
+
+/**
+ * Implements hook_form_FORM_alter().
+ */
+function unc_charlotte4_form_system_theme_settings_alter(&$form, $form_state) {
+  // Collapse theme settings that should not be changed.
+  $form['logo']['#collapsible'] = $form['theme_settings']['#collapsible'] = $form['favicon']['#collapsible'] = TRUE;
+  $form['logo']['#collapsed'] = $form['theme_settings']['#collapsed'] = $form['favicon']['#collapsed'] = TRUE;
+
+  $form['unc_charlotte4_theme_options'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('unc_charlotte4 Theme Options'),
+    '#collapsible' => TRUE,
+    '#collapsed' => FALSE,
+    '#weight' => -11,
+  );
+  $form['unc_charlotte4_theme_options']['subbrand_text'] = array(
+    '#type' => 'textfield',
+    '#title' => t('College / Division Sub-brand Name'),
+    '#description' => t("The name of this website's parent organization or a tagline."),
+    '#default_value' => theme_get_setting('subbrand_text'),
+  );
+  $form['unc_charlotte4_theme_options']['subbrand_link'] = array(
+    '#type' => 'textfield',
+    '#title' => t('College / Division Sub-brand Link'),
+    '#description' => t("Enter the URL of the parent organization's website or leave blank for a tagline."),
+    '#default_value' => theme_get_setting('subbrand_link'),
+  );
+  $form['unc_charlotte4_theme_options']['utility_link_3_text'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Optional 3rd Utility Link Text'),
+    '#description' => t("Optional 3rd utility link text. If this site utilizes academic advising, then enter 'Academic Advising'."),
+    '#default_value' => theme_get_setting('utility_link_3_text'),
+  );
+  $form['unc_charlotte4_theme_options']['utility_link_3_link'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Optional 3rd Utility Link'),
+    '#description' => t("Optional 3rd utility link's URL. If this site utilizes academic advising, enter the advising site's URL here."),
+    '#default_value' => theme_get_setting('utility_link_3_link'),
+  );
+  $form['unc_charlotte4_theme_options']['utility_link_4_text'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Optional 4th Utility Link Text'),
+    '#description' => t("Optional 4th utility link text. If this site utilizes a search box, then this link won't be shown."),
+    '#default_value' => theme_get_setting('utility_link_4_text'),
+  );
+  $form['unc_charlotte4_theme_options']['utility_link_4_link'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Optional 4th Utility Link'),
+    '#description' => t("Optional 4th utility link's URL. If this site utilizes a search box, then this link won't be shown."),
+    '#default_value' => theme_get_setting('utility_link_4_link'),
+  );
+  $form['unc_charlotte4_theme_options']['search_box_type'] = array(
+    '#type' => 'radios',
+    '#title' => t('Search Box Type'),
+    '#description' => t("Specify if the search in the upper-right of the page should use the built-in Drupal search utility or to use Google to search this site."),
+    '#options' => array('drupal' => t('Drupal Search'), 'google' => t('Google Search'), 'disabled' => t('Disabled')),
+    '#default_value' => theme_get_setting('search_box_type') ? theme_get_setting('search_box_type') : 'drupal',
+  );
+  $form['unc_charlotte4_theme_options']['nav_style'] = array(
+    '#type' => 'select',
+    '#title' => t('Navigation Style'),
+    '#description' => t("Specify the style for the primary navigation. Tabbed should only be chosen when a slideshow will be provided above menu on every page."),
+    '#options' => array('tabs' => t('Tabbed'), 'leftnav' => t('Left Nav'), 'default' => t('Default')),
+    '#default_value' => theme_get_setting('nav_style') ? theme_get_setting('nav_style') : 'default',
+  );
+  $menu_options = menu_get_menus();
+  $form['unc_charlotte4_theme_options']['mobile_menu'] = array(
+    '#type' => 'select',
+    '#title' => t('Mobile Menu'),
+    '#description' => t("Specify the menu to use as the collapsible primary on mobile devices."),
+    '#options' => $menu_options,
+    '#default_value' => theme_get_setting('mobile_menu') ? theme_get_setting('mobile_menu') : 'main-menu',
+  );
+}
