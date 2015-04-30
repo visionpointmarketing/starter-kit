1b2b8d280f6f9ff5a365a7557e8c879821e4deaa
Steven Cobb <stevecobb91@gmail.com>
2015-04-30 18:03:27 -0400

    QA fixes to home and join band

--- themes/unc_charlotte4/templates/bean--text-and-links-promo.tpl.php
+++ themes/unc_charlotte4/templates/bean--text-and-links-promo.tpl.php
@@ -38,7 +38,7 @@
 			<?php print render($content['field_intro_text']); ?>
 			<?php if(isset($content['field_list_of_links']) && $content['field_list_of_links'] != ''){ ?>
 				<div class="list-links">
-					<h2 class="block__title">Select A Page</h2>
+					<h2 class="block__title">Select a Page</h2>
 					<?php print render($content['field_list_of_links']); ?>
 				</div>
 				<?php } ?>
